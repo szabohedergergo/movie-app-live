7bc31b0d011f2e32c2976bc95e6df393ac93e716
Gergo Szabo <szabohedergergo@gmail.com>
2025-04-08 12:59:17 +0200

    hello worldsa

--- movie-app-live/ContentView.swift
+++ movie-app-live/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.medium)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello, worlsd!")
         }
         .padding()
     }

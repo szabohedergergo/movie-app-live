3716da9d71a61ea613c4f7dcf7ab52394435ce8b
Gergo Szabo <szabohedergergo@gmail.com>
2025-04-08 12:44:59 +0200

    new

--- movie-app-live/ContentView.swift
+++ movie-app-live/ContentView.swift
@@ -11,7 +11,7 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
-                .imageScale(.large)
+                .imageScale(.medium)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
         }

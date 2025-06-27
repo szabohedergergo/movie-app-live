89ffee01f12737e84d7e2595f8747c01a790d550
Gergo Szabo <szabohedergergo@gmail.com>
2025-06-28 01:14:37 +0200

    test, reviews

---
+++ movie-app-liveUITests/ui/favorites/FavoriesView+UI.swift
@@ -0,0 +1,42 @@
+//
+//  FavoriesView+UI.swift
+//  movie-app-live
+//
+//  Created by Zsolt Pete on 2025. 06. 17..
+//
+
+import XCTest
+
+final class FavoriesViewUITests: XCTestCase {
+    
+    let app = XCUIApplication()
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        
+        app.launch()
+        sleep(3)
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    func testFavoriteSelection() throws {
+        app.images["favorites"].tap()
+        let scrollView = app.firstScrollViewView(withIdentifier: AccessibilityLabels.favoritesScrollView)
+        scrollView.swipeUp()
+        print(scrollView.images)
+        
+        let secondCell = app.findElement(withId: "MediaItem2")
+        secondCell?.tap()
+         
+                
+        
+    }
+}
---
+++ movie-app-liveUITests/ui/search/SearchView+UI.swift
@@ -0,0 +1,39 @@
+//
+//  SearchView+UI.swift
+//  movie-app-live
+//
+//  Created by Zsolt Pete on 2025. 06. 17..
+//
+
+import XCTest
+
+final class SearchViewUITests: XCTestCase {
+    
+    let app = XCUIApplication()
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        
+        app.launch()
+        sleep(3)
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    func testSearchTextField() throws {
+        app.images["search"].tap()
+        let searchTextField = app.findElement(withId: AccessibilityLabels.searchTextField)
+        searchTextField?.tap()
+        searchTextField?.typeText("Fight club")
+        sleep(4)
+                
+        
+    }
+}
